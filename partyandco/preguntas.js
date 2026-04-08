d333e7749233b6b944cee6f9909b28fb4c35f287
Nrpp <143441528+Nrpp@users.noreply.github.com>
2026-04-08 15:42:36 +0200

    Create preguntas.js

---
+++ partyandco/preguntas.js
@@ -0,0 +1,29 @@
+const preguntas = {
+    dibujar: [
+        "Dibuja un piano",
+        "Dibuja a Chopin",
+        "Dibuja una partitura"
+    ],
+    mimica: [
+        "Imita tocar el piano",
+        "Imita un concierto",
+        "Imita estar triste como Chopin"
+    ],
+    definicion: [
+        "¿Qué es un nocturno?",
+        "Define música romántica",
+        "¿Qué es una partitura?"
+    ],
+    test: [
+        {
+            pregunta: "¿Dónde nació Chopin?",
+            opciones: ["Francia", "Polonia", "Italia", "Alemania"],
+            correcta: 1
+        },
+        {
+            pregunta: "¿Qué instrumento tocaba?",
+            opciones: ["Violín", "Piano", "Flauta", "Guitarra"],
+            correcta: 1
+        }
+    ]
+};
